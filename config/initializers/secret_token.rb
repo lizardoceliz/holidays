f4f77ec4db3f1d2b5a08eeefea851f5914a77c9b
Alejandro <Alejandro@MacBook.local>
2011-12-16 14:09:07 -0300

    Primer commit - estructura de directorios vacia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holidays::Application.config.secret_token = 'a347a1b525c76cf46a6f08e5927dbb2e3e6148c129d3e5f90bc7245a77fc0bc54a71776d4ec31bc7510a2ccb0ff9c17b14bd76a31ce324dc48b5ce5fa17181c0'
